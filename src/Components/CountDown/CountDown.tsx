0c678d0bb8402eb3bd7b434b497cf09411925ac3
Ahmedshk <ahmedsheikh540@gmail.com>
2022-05-24 20:27:17 +0500

    all done

--- src/Components/CountDown/CountDown.tsx
+++ src/Components/CountDown/CountDown.tsx
@@ -57,125 +57,125 @@ const CountDown = () => {
         const rightJustNow = new Date().getTime();
         const runway = targetDate - rightJustNow;
         const stateObj = {
-            days: Math.floor(runway / (1000  60  60 * 24)),
-            hours: Math.floor((runway % (1000  60  60  24)) / (1000  60 * 60)),
-        minutes: Math.floor((runway % (1000  60  60)) / (1000 * 60)),
-        seconds: Math.floor((runway % (1000 * 60)) / 1000)
+            days: Math.floor(runway / (1000 * 60 * 60 * 24)),
+            hours: Math.floor((runway % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)),
+            minutes: Math.floor((runway % (1000 * 60 * 60)) / (1000 * 60)),
+            seconds: Math.floor((runway % (1000 * 60)) / 1000)
+        };
+        return stateObj;
     };
-    return stateObj;
-};
 
-const [timeDisplay, setTimeDisplay] = React.useState<TimeDisplayValuesType>(
-    generateTimeDisplay
-);
+    const [timeDisplay, setTimeDisplay] = React.useState<TimeDisplayValuesType>(
+        generateTimeDisplay
+    );
 
-React.useEffect(() => {
-    setInterval(() => setTimeDisplay(generateTimeDisplay), 1000);
-}, []);
+    React.useEffect(() => {
+        setInterval(() => setTimeDisplay(generateTimeDisplay), 1000);
+    }, []);
 
-return (
-    <div className="main_container">
-        <Container>
-            <Row>
-                <Col md={12}>
-                    <div className='moon_landing_container'>
-                        <h3>MOON LANDING PARTY</h3>
-                        <h1>{todaysDate}</h1>
-                        <span className="anniversary_starts">Anniversary starts in</span>
-                        <Row className="mt-3 justify-content-center align-items-center">
-                            <Col xs={2} md={2} className="d-flex justify-content-center">
-                                <div className="counter_container">
-                                    <p className="mb-0">{timeDisplay.days}</p>
-                                    <span>Days</span>
-                                </div>
-                            </Col>
-                            <Col xs={1} md={1} className="colon_container">
-                                <span>:</span>
-                            </Col>
+    return (
+        <div className="main_container">
+            <Container>
+                <Row>
+                    <Col md={12}>
+                        <div className='moon_landing_container'>
+                            <h3>MOON LANDING PARTY</h3>
+                            <h1>{todaysDate}</h1>
+                            <span className="anniversary_starts">Anniversary starts in</span>
+                            <Row className="mt-3 justify-content-center align-items-center">
+                                <Col xs={2} md={2} className="d-flex justify-content-center">
+                                    <div className="counter_container">
+                                        <p className="mb-0">{timeDisplay.days}</p>
+                                        <span>Days</span>
+                                    </div>
+                                </Col>
+                                <Col xs={1} md={1} className="colon_container">
+                                    <span>:</span>
+                                </Col>
 
-                            <Col xs={2} md={2} className="d-flex justify-content-center">
-                                <div className="counter_container">
-                                    <p className="mb-0">{timeDisplay.hours}</p>
-                                    <span>Hours</span>
-                                </div>
-                            </Col>
-                            <Col xs={1} md={1} className="colon_container">
-                                <span>:</span>
-                            </Col>
+                                <Col xs={2} md={2} className="d-flex justify-content-center">
+                                    <div className="counter_container">
+                                        <p className="mb-0">{timeDisplay.hours}</p>
+                                        <span>Hours</span>
+                                    </div>
+                                </Col>
+                                <Col xs={1} md={1} className="colon_container">
+                                    <span>:</span>
+                                </Col>
 
-                            <Col xs={2} md={2} className="d-flex justify-content-center">
-                                <div className="counter_container">
-                                    <p className="mb-0">{timeDisplay.minutes}</p>
-                                    <span>Minutes</span>
-                                </div>
-                            </Col>
-                            <Col md={1} xs={1} className="colon_container">
-                                <span>:</span>
-                            </Col>
+                                <Col xs={2} md={2} className="d-flex justify-content-center">
+                                    <div className="counter_container">
+                                        <p className="mb-0">{timeDisplay.minutes}</p>
+                                        <span>Minutes</span>
+                                    </div>
+                                </Col>
+                                <Col md={1} xs={1} className="colon_container">
+                                    <span>:</span>
+                                </Col>
 
-                            <Col xs={2} md={2} className="d-flex justify-content-center">
-                                <div className="counter_container">
-                                    <p className="mb-0">{timeDisplay.seconds}</p>
-                                    <span>Seconds</span>
-                                </div>
-                            </Col>
+                                <Col xs={2} md={2} className="d-flex justify-content-center">
+                                    <div className="counter_container">
+                                        <p className="mb-0">{timeDisplay.seconds}</p>
+                                        <span>Seconds</span>
+                                    </div>
+                                </Col>
 
-                        </Row>
-                        <div className='text-center mt-3'>
-                            <button onClick={() => setShow(true)}>Share Event</button>
+                            </Row>
+                            <div className='text-center mt-3'>
+                                <button onClick={() => setShow(true)}>Share Event</button>
+                            </div>
+                        </div>
+                    </Col>
+                </Row>
+
+                {/* Modal */}
+
+                <Modal show={show} size={"undefined"} handleClose={handleClose}>
+                    <div className="modal_data">
+                        <p>Share this event with your social community</p>
+                        <div className="icons_container">
+
+                            <div className="twitter_container">
+                                <TwitterShareButton title={"Who else is going to the moon party?"} url={"https://moonparty.com"}>
+                                    <img src={twitterIcon} />
+                                </TwitterShareButton>
+                            </div>
+
+
+                            <div className="fb_container">
+                                <FacebookShareButton
+                                    url={"https://moonparty.com"}
+                                    quote={"Who else is going to the moon party?"}
+                                >
+                                    <img src={FBIcon} />
+                                </FacebookShareButton>
+
+                            </div>
+
+                        </div>
+                        <span>or copy link</span>
+
+                        <div className="clipboard_container">
+                            <div className="d-flex">
+                                <span className='m-0'><BsLink45Deg /></span>
+                                <p> https://moonparty.com</p>
+                            </div>
+                            <div>
+                                <CopyToClipboard text={'https://moonparty.com'}
+                                    onCopy={() => setCopySuccess({ copied: true })}>
+                                    <button>
+                                        {copySuccess.copied ? "Link Copied!" :
+                                            "Copy"
+                                        }
+                                    </button>
+                                </CopyToClipboard>
+                            </div>
                         </div>
                     </div>
-                </Col>
-            </Row>
-
-            {/ Modal /}
-
-            <Modal show={show} size={"undefined"} handleClose={handleClose}>
-                <div className="modal_data">
-                    <p>Share this event with your social community</p>
-                    <div className="icons_container">
-
-                        <div className="twitter_container">
-                            <TwitterShareButton title={"Who else is going to the moon party?"} url={"https://moonparty.com"}>
-                                <img src={twitterIcon} />
-                            </TwitterShareButton>
-                        </div>
-
-
-                        <div className="fb_container">
-                            <FacebookShareButton
-                                url={"https://moonparty.com"}
-                                quote={"Who else is going to the moon party?"}
-                            >
-                                <img src={FBIcon} />
-                            </FacebookShareButton>
-
-                        </div>
-
-                    </div>
-                    <span>or copy link</span>
-
-                    <div className="clipboard_container">
-                        <div className="d-flex">
-                            <span className='m-0'><BsLink45Deg /></span>
-                            <p> https://moonparty.com</p>
-                        </div>
-                        <div>
-                            <CopyToClipboard text={'https://moonparty.com'}
-                                onCopy={() => setCopySuccess({ copied: true })}>
-                                <button>
-                                    {copySuccess.copied ? "Link Copied!" :
-                                        "Copy"
-                                    }
-                                </button>
-                            </CopyToClipboard>
-                        </div>
-                    </div>
-                </div>
-            </Modal>
-        </Container>
-    </div>
-)
+                </Modal>
+            </Container>
+        </div>
+    )
 }
 
 export default CountDown
